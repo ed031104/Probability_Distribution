9006cd13eed43d301dd72b4afb6ddb5aa531eace
edwin <edwin.noviembre.0306@gmail.com>
2024-06-16 19:20:25 -0600

    cambios

---
+++ readme.md
@@ -0,0 +1,29 @@
+Pip freeze > requirements.txt
+
+## Description:
+
+A brief description of what this project does and who it's for
+
+This project was made in order to be able to convert any video format to mp4 with python, occupying the "moviepy" library
+
+## Installation
+
+First, create a virtual environment:
+
+
+```bash
+python -m venv Virtual_Environment_Name
+```
+
+
+Then activate your virtual environment
+
+```bash
+cd Virtual_Environment_Name/Script/activate
+```
+
+Install the dependencies needed to run this project with pip
+
+```bash
+  pip install -r requirements.txt
+```
--- src/main.py
+++ src/main.py
@@ -9,12 +9,10 @@ mediaM = float(input("Ingresa la Media Muestral = "))
 desvE = float(input("Ingresa la Desviacion Estandar = "))
 mediaH = float(input("Ingresa la Media Hipotética = ")) 
 
-data = [
-    ["Nivel de Significancia", "Tamaño de la muestra", "Media Muestral", "Desviacion Estandar", "Media Hipotética"],
-    [Ns, muestra, mediaM, desvE, mediaH]
-    ]
-
+header = ["Nivel de Significancia", "Tamaño de la muestra", "Media Muestral", "Desviacion Estandar", "Media Hipotética"]
+data = [Ns, muestra, mediaM, desvE, mediaH]
+    
 StudentService.DistribucionTStudent(Ns, muestra, mediaM, desvE, mediaH)
 
-print(tabulate(data, headers=[''], tablefmt='grid'))
+print(tabulate(data, headers=header, tablefmt='rounded_outline'))
 
